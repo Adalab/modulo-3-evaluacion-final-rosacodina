59103c957648c566952428d523bd3d1c648166d2
Rosa Codina <rosa.codina90@gmail.com>
2022-05-04 21:05:07 +0200

    MoviesApi error is solved

--- src/components/App.js
+++ src/components/App.js
@@ -6,30 +6,23 @@
 
 import '../styles/App.scss';
 import getApiData from '../services/moviesApi'; //I can name this import as I prefer
+import { useEffect } from 'react';
 
 // Función principal.
 function App() {
-  // Variables generales, constantes, variables de estado.
-  // Hooks.
-  // Funciones genrales, funnciones manejadoras de eventos.
+  const [dataMovies, setDataMovies] = useState([]);
+
+  useEffect(() => {
+    getApiData().then((dataClean) => {
+      console.log(dataClean);
+      setDataMovies(dataClean);
+    });
+  }, []);
 
   return (
     // Como solo se puede devolver una etiqueta, se puede usar una etiqueta ficticia vacia, llamada FRAGMENTO, y que se escribe, abiriendo y cerrando la etiqueta vacia, así "<> html </>".
     <>
-      <body>
-        <header>
-          <h1 className="header__title">Owen Wilson's "wow" films</h1>
-        </header>
-
-        <main>
-          <form>
-            <input>Movie</input>
-            <select>
-              <option value="volvo">Volvo</option>
-            </select>
-          </form>
-        </main>
-      </body>
+      <h1 className="header__title">Owen Wilson's "wow" films</h1>
     </>
   );
 }
