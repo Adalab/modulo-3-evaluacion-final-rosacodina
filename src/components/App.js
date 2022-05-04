e530165106452d6e3478a8e32979739b1c1a2f44
Rosa Codina <rosa.codina90@gmail.com>
2022-05-04 19:48:13 +0200

    Repository done and small structure

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+// import sirve para enlazar /traer las dependencias que necesitamos.
+// Librerías JS
+// CSS
+// Imágenes
+// JSON
+
+import '../styles/App.scss';
+
+// Función principal.
+function App() {
+  // Variables generales, constantes, variables de estado.
+  // Hooks.
+  // Funciones genrales, funnciones manejadoras de eventos.
+
+  return (
+    // Como solo se puede devolver una etiqueta, se puede usar una etiqueta ficticia vacia, llamada FRAGMENTO, y que se escribe, abiriendo y cerrando la etiqueta vacia, así "<> html </>".
+    <>
+      <body>
+        <header>
+          <h1 className="header__title">Owen Wilson's "wow" films</h1>
+        </header>
+
+        <main>
+          <form>
+            <input>Movie</input>
+            <select>
+              <option value="volvo">Volvo</option>
+            </select>
+          </form>
+        </main>
+      </body>
+    </>
+  );
+}
+
+export default App;
