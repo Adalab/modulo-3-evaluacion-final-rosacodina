bed47fb4933dce4b056a6ce256d0f599ba51483e
Rosa Codina <rosa.codina90@gmail.com>
2022-05-08 13:52:10 +0200

    HashRouter added on index.js file but I cannot solve the errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
+
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
