5486cbaeac7ae0ba7f8fd3e5c8d1bc943aaf9d86
Rosa Codina <rosa.codina90@gmail.com>
2022-05-09 11:35:10 +0200

    Node modules deleted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import './styles/index.css';
 import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
